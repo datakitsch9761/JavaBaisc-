8fa8fb27c391bd12784ad921e6db88930d934f1d
datakitsch9761 <data.kitsch@gmail.com>
2021-03-25 18:06:08 +0900

    Update from Yesterday

---
+++ src/joseph/basic/joseph/basic/day07/B.java
@@ -0,0 +1,12 @@
+package joseph.basic.joseph.basic.day07;
+
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
+public class B {
+    int d; //default
+    private int e; //private
+    public int f; //public
+    //
+
+    B two = new B();
+}
--- src/joseph/basic/joseph/basic/day07/EX05.java
+++ src/joseph/basic/joseph/basic/day07/EX05.java
@@ -3,4 +3,5 @@ package joseph.basic.joseph.basic.day07;
 public class EX05 {
     public static void main(String[] args) {
     }
+
 }
