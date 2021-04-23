cdb461295876cc0e554024f48e7cdc05882d52a5
datakitsch9761 <data.kitsch@gmail.com>
2021-04-23 12:52:44 +0900

    개시판 프로그래밍 파트1 (추가, 조회, 상세조회)

---
+++ src/joseph/basic/day19/BoardDAO.java
@@ -0,0 +1,13 @@
+package joseph.basic.day19;
+
+import java.util.List;
+
+public interface BoardDAO {
+    int insertBoard(BoardVO bvo);
+    List<BoardVO> selectBoard();
+    BoardVO selectOneBoard(int bdno);
+    int updateBoard(BoardVO bvo);
+    int deleteBoard(int bdno);
+
+    void thumupBoard(int bdno);
+}
---
+++ src/joseph/basic/day19/BoardMain.java
@@ -0,0 +1,18 @@
+package joseph.basic.day19;
+
+public class BoardMain {
+    public static void main(String[] args) {
+    //Step 1
+    //BoardService 객체부터 생성
+        BoardService bsrv = BoardServiceimpl.getInstance();
+    //데이터 입력
+        bsrv.newBoard();
+
+    //데이터 조회
+        bsrv.readBoard();
+    //데이터 상세조회
+        bsrv.readOneBoard();
+    //데이터 수정
+    //데이터 삭제
+    }
+}
